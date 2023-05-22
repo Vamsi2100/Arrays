f6dd78347dd4b3e8616d240cf8af3ae276f1614a
Vamsi2100 <Vamsifighter2108@gmail.com>
2023-05-22 17:03:43 +0530

    Added: Printing reverse order of array

---
+++ ReverseOrder.java
@@ -0,0 +1,16 @@
+public class ReverseOrder {
+    public static void main(String[] args) {
+        int array [] = {1, 2, 3, 4, 5};
+        System.out.println("Original Array");
+        for (int i = 0; i < array.length; i++){
+            System.out.println(array[i] );
+        }
+
+        System.out.println("Reverse Order Array");
+        for (int i = array.length-1; i >= 0; i--) {
+
+            System.out.println(array[i]);
+        }
+
+    }
+}
