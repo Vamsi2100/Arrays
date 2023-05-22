de8c89c68002964c8727fab2a48c25fa59bb2b3d
Vamsi2100 <Vamsifighter2108@gmail.com>
2023-05-22 12:37:58 +0530

    Added: print elements in a array file

---
+++ ElementsArray.java
@@ -0,0 +1,10 @@
+public class ElementsArray {
+    public static void main(String[] args) {
+        int array [ ] = {1, 2, 3, 4,};
+        for (int i = 0; i<array.length; i++){
+            System.out.println(array[i]);
+
+        }
+
+    }
+}
